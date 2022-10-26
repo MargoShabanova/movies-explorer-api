7eee78ec64e77259a2ff602a21106b67ed6ecff9
Margarita Shabanova <marg.alexandrova2018@yandex.ru>
2022-10-26 18:49:39 +0300

    fix: edit validation

--- middlewares/validaton.js
+++ middlewares/validaton.js
@@ -41,7 +41,7 @@ const newMovieValidation = celebrate({
 
 const deleteMovieValidation = celebrate({
   params: Joi.object().keys({
-    objectId: Joi.string().required().hex().length(24),
+    movieId: Joi.string().required().hex().length(24),
   }),
 });
 
