0ba5cfe363af86112a15ea5e525e85133529e0f4
Margarita Shabanova <marg.alexandrova2018@yandex.ru>
2022-11-02 13:52:01 +0300

    fix: edit status

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,7 +4,7 @@ module.exports.errorHandler = ((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res
-    .status(err.statusCode)
+    .status(statusCode)
     .send({
       message: statusCode === 500
         ? INTERNAL_SERVER_ERROR_MESSAGE
